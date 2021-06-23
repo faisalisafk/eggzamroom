8baa8e9df1fa9254fa19bba316577c20cd56015f
faisalnsu <faisal.hossain03@northsouth.edu>
2021-06-23 23:06:26 +0600

    fetching user pk in teacher dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,7 @@ def login_view(request):
     user = request.user
     context = {}
     if user.is_authenticated:
-        return redirect('home')
+        return redirect('/teacher')
 
     if request.POST:
         form = AccountAuthenticationForm(request.POST)
@@ -18,7 +18,7 @@ def login_view(request):
             user = authenticate(email=email, password=password)
             if user:
                 login(request, user)
-                return redirect('home')
+                return redirect('/teacher')
     else:
         form = AccountAuthenticationForm()
 
--- teacher/views.py
+++ teacher/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {}
-    return render(request, 'teacher/teacher_courses.html', context)
+    user = request.user
+
+    if user.is_authenticated:
+        context = {'user_id': user.pk}
+        return render(request, 'teacher/teacher_courses.html', context)
+
+
