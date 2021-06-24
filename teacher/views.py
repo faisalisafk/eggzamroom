09c79957c507cd906e05a64af75f94dc30e524f5
faisalnsu <faisal.hossain03@northsouth.edu>
2021-06-24 12:42:26 +0600

    added some basic files

--- teacher/views.py
+++ teacher/views.py
@@ -8,12 +8,11 @@ def index(request):
     if user.is_authenticated:
         context = {'user_id':user.pk}
         #normal import not working so this way managed
-        Teacher = apps.get_model('accounts', 'Teacher')
-        temp = Teacher.objects.get(pk=user.pk)
-        val = Teacher.objects.values()
-        print(temp)
-        for t in val:
-            print(t)
+        User = apps.get_model('accounts','User')
+        temp = User.objects.get(pk=user.pk)
+
+        #logged in user detail in user
+        context = {'user':temp}
         return render(request, 'teacher/teacher_courses.html', context)
 
 
