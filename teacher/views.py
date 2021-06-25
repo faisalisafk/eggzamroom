91040db8630fe24e5569dc2dadfee6614f9555c5
faisalnsu <faisal.hossain03@northsouth.edu>
2021-06-26 00:19:16 +0600

    added Modal in Create Course

--- teacher/views.py
+++ teacher/views.py
@@ -4,18 +4,20 @@ from django.apps import apps
 from accounts.models import User
 from .models import Course
 
-def index(request):
+def dashboard(request):
     user = request.user
 
     if user.is_authenticated:
         context = {'user_id': user.pk}
-        # normal import not working so this way managed
+
 
         userDetails = User.objects.get(pk=user.pk)
         courseList = Course.objects.filter(teacher=user.pk)
-        # logged in user detail in user
 
-        print(courseList)
+
         context = {'user': userDetails,
                    'course': courseList,}
         return render(request, 'teacher/teacher_courses.html', context)
+
+def addCourse(request):
+    pass
