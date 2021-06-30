367731d27072c3b9e3ecb2024890d7db6ddd0649
faisalnsu <faisal.hossain03@northsouth.edu>
2021-06-30 16:09:22 +0600

    added base and footer, changed modal UI

--- teacher/forms.py
+++ teacher/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class CourseForm(forms.Form):
-    title = forms.CharField(label='Course Title', max_length=30)
-    subject = forms.CharField(label='Subject Name', max_length=30)
+    title = forms.CharField(label='Course Title', max_length=30,strip=True)
+    subject = forms.CharField(label='Subject Name', max_length=30,strip=True)
 
 class ExamForm(forms.Form):
-    title = forms.CharField(label='Exam Title',max_length=30)
-    description = forms.CharField(label='Exam Description',max_length=50)
+    title = forms.CharField(label='Exam Title',max_length=30,strip=True)
+    description = forms.CharField(label='Exam Description',max_length=50,strip=True)
