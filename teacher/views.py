beb20dd367c4369604dc23ea237cdb874b86737e
Ahmed_1901 <ahmed.jawad01@northsouth.edu>
2021-06-30 09:37:36 +0600

    added cards to teacher_courses.html

--- teacher/views.py
+++ teacher/views.py
@@ -1,10 +1,11 @@
 import requests
-from django.shortcuts import render,redirect,HttpResponse,HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
 
 from django.apps import apps
-from accounts.models import User,Teacher
-from .models import Course,Exam
-from .forms import CourseForm,ExamForm
+from accounts.models import User, Teacher
+from .models import Course, Exam
+from .forms import CourseForm, ExamForm
+
 
 def dashboard(request):
     if request.user.is_authenticated:
@@ -14,8 +15,8 @@ def dashboard(request):
                 if form.is_valid():
                     title = form.cleaned_data['title']
                     subject = form.cleaned_data['subject']
-                    teacher = Teacher.objects.get(pk = request.user.pk)
-                    c = Course(title=title,subject=subject,teacher=teacher)
+                    teacher = Teacher.objects.get(pk=request.user.pk)
+                    c = Course(title=title, subject=subject, teacher=teacher)
                     c.save()
                     return redirect('/teacher/')
                 else:
@@ -26,21 +27,22 @@ def dashboard(request):
                 courseList = Course.objects.filter(teacher=request.user.pk)
                 context = {'user': userDetails,
                            'course': courseList,
-                           'form': form }
+                           'form': form}
                 return render(request, 'teacher/teacher_courses.html', context)
         else:
             return redirect('/logout/')
     else:
         return redirect('/login/')
 
-def coursePage(request,coursePk):
+
+def coursePage(request, coursePk):
     if request.method == 'POST':
         form = ExamForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data['title']
             description = form.cleaned_data['description']
-            course = Course.objects.get(pk = coursePk)
-            exam = Exam(title = title, description= description,course = course)
+            course = Course.objects.get(pk=coursePk)
+            exam = Exam(title=title, description=description, course=course)
             exam.save()
             return HttpResponseRedirect(request.path_info)
         else:
@@ -48,9 +50,7 @@ def coursePage(request,coursePk):
 
     else:
         form = ExamForm()
-        exams = Exam.objects.filter(course = coursePk)
-        context = {'exams':exams,
-                   'form':form}
-        return render(request,'teacher/exams.html',context)
-
-
+        exams = Exam.objects.filter(course=coursePk)
+        context = {'exams': exams,
+                   'form': form}
+        return render(request, 'teacher/exams.html', context)
