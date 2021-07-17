b0f52332588db1215cf8ae8a88e6410e6d4ddb05
faisalnsu <faisal.hossain03@northsouth.edu>
2021-07-17 17:08:25 +0600

    ajax and jquery implementation

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Course,Exam
+from .models import Course,Exam, Form
 
 admin.site.register(Course)
 admin.site.register(Exam)
+admin.site.register(Form)
--- teacher/urls.py
+++ teacher/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('course/<int:coursePk>/', views.coursePage, name='coursePage'),
     path('exam/<int:examPk>/',views.formPage,name='formPage'),
+    path('exam/<int:examPk>/save',views.saveForm,name='save'),
 ]
