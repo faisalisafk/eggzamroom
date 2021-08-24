8e88eaaf0c82a3765f6f89b547c1040b3b1a238e
faisalnsu <faisal.hossain03@northsouth.edu>
2021-08-24 20:16:43 +0600

    minor bug fixed

--- student/urls.py
+++ student/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('course/<int:coursePk>/', views.coursePage, name='coursePage'),
     path('exam/<int:examPk>/', views.examFormPage, name='examFormPage'),
     path('exam/<int:examPk>/saveAnswer', views.saveAnswer, name='saveAnswer'),
-    path('exam/<int:examPk>/detectionlog', views.WindowDetectionLog, name='detectionlog')
+    path('exam/<int:examPk>/detectionlog', views.WindowDetectionLog, name='detectionlog'),
+    path('<int:formPk>/submit',views.submit,name='submit'),
 ]
