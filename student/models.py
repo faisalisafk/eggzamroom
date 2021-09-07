8834f95da4f82fee9cbf534058f320b1e7b39f38
Ahmed_1901 <jawad1901@gmail.com>
2021-09-07 11:19:04 +0600

    -fix log of student in teacher's view_score -added darkmode themer

--- student/models.py
+++ student/models.py
@@ -2,27 +2,33 @@ import uuid
 from django.db import models
 from django.db.models.base import Model
 from accounts.models import User
-from teacher.models import Course, Form, Question,Choice
+from teacher.models import Course, Form, Question, Choice
 
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     studentID = models.IntegerField(unique=True, null=True)
     course = models.ManyToManyField(Course)
+
     # exams = models.ManyToManyField(Exam, through='TakenExam')
 
     def __str__(self):
         return self.user.firstName + " " + self.user.lastName
 
+
 class Answer(models.Model):
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    form = models.ForeignKey(Form,on_delete=models.CASCADE,blank=True,null=True)
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    givenAnswer = models.ForeignKey(Choice,on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, blank=True, null=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    givenAnswer = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
 
 class SubmittedForm(models.Model):
-    student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    form = models.ForeignKey(Form,on_delete=models.CASCADE,blank=True,null=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.student.user.firstName
 
 
 class StudentWindowDetectionLog(models.Model):
